85d0522040e358fd7bbc559575cc8956c0580360
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-04 13:37:05 +0200

    fixed new line in bot message

--- config/dmTalkPluginConfiguration.class.php
+++ config/dmTalkPluginConfiguration.class.php
@@ -39,7 +39,7 @@ class dmTalkPluginConfiguration extends sfPluginConfiguration
     {
       if(class_exists('dmFrontLinkTagPage'))
       {
-        $speaker->say(str_replace(':', ":\n", $this->i18n->__("Give this url to invite someone to join the room: %url%", array(
+        $speaker->say(str_replace('http', "\nhttp", $this->i18n->__("Give this url to invite someone to join the room: %url%", array(
           '%url%' => $this->context->getHelper()->link($this->context->getPage())->param('r', $speaker->Room->code)->getAbsoluteHref())
         )));
       }
